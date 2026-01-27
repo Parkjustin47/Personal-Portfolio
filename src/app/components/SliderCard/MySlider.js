96bdd9d3ebf89c3c7ef102e37562a3eccd1d896a
ParkJustin47 <Parkjustin47@gmail.com>
2026-01-26 20:00:41 -0800

    Fixed project display ui

--- src/app/components/SliderCard/MySlider.js
+++ src/app/components/SliderCard/MySlider.js
@@ -7,19 +7,19 @@ import 'slick-carousel/slick/slick-theme.css'
 
 export default function MySlider() {
   const settings = {
-    dots: true,
-    infinite: true,
+    dots: false,
+    infinite: false,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 3000,
     pauseOnHover: true,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 1,
           slidesToScroll: 1,
         }
       },
